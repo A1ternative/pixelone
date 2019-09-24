d61b35efccad51956580a9f0f7eb16606a8a3c5a
A1ternative <alternative6556@gmail.com>
2019-09-24 23:56:33 +0400

    add animation character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,32 +11,54 @@ public class Player : MonoBehaviour
     public float minimalHeigth;
     public bool isCheatMode;
     public GroundDetection groundDetection;
-
-           
+    private Vector3 direction;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    private bool isJumping;
+             
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
     {
-        
+        animator.SetBool("isGrounded", groundDetection.isGrounded);
+        if (!isJumping && !groundDetection.isGrounded)
+            animator.SetTrigger("StartFall");
+        isJumping = isJumping && !groundDetection.isGrounded;
+        // transform.Translate(Vector2.right * Time.deltaTime * speed);
+        direction = Vector3.zero; // (0;0)
         if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector2.left * Time.deltaTime * speed);
-        }
+            direction = Vector3.left; //(-1; 0)
         if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * speed);
-        }
+            direction = Vector3.right; // (1;0)
+        direction *= speed;
+        direction.y = rigidBody.velocity.y;
+        rigidBody.velocity = direction;
+
         if (Input.GetKeyDown(KeyCode.Space) && groundDetection.isGrounded)
         {
             rigidBody.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+            animator.SetTrigger("StartJump");
+            isJumping = true;
         }
 
-        //if (transform.position.y < minimalHeigth && isCheatMode)
-        //{
-        //    rigidBody.position = new Vector2(0, 0);
-        //    transform.position = new Vector3(-30, 36, 0);
-        //}
-        //if (transform.position.y < minimalHeigth && !isCheatMode)
-        //    Destroy(gameObject);
+        animator.SetFloat("Speed", Mathf.Abs(direction.x));
+
+        if (direction.x > 0)
+            spriteRenderer.flipX = false;
+        if (direction.x < 0)
+            spriteRenderer.flipX = true;
+
+        CheckFall();
+    }
+
+    void CheckFall()
+    {
+        if (transform.position.y < minimalHeigth && isCheatMode)
+        {
+            rigidBody.position = new Vector2(-30, -3);
+            transform.position = new Vector3(-30, -4, 0);
+        }
+        if (transform.position.y < minimalHeigth && !isCheatMode)
+            Destroy(gameObject);
     }
 }
