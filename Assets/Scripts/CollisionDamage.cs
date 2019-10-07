447b4e5661f1fb11b3625c5ee4fd0437e53c91c5
A1ternative <alternative6556@gmail.com>
2019-10-07 22:57:38 +0400

    lesson 6 video 3

--- Assets/Scripts/CollisionDamage.cs
+++ Assets/Scripts/CollisionDamage.cs
@@ -7,26 +7,26 @@ public class CollisionDamage : MonoBehaviour
     [SerializeField] private int damage = 10;
     [SerializeField] private string collisionTag; // tag игровых объектов с которыми мы (объект на котором размещен скрипт) будем взаимодействовать
     [SerializeField] private Animator animator;
-    private Health targetHealth; // можно целиком сделать приватным, нет необходимости его как то изменять в инспекторе (пока ситуация такого не требует)
+    private Health health; // можно целиком сделать приватным, нет необходимости его как то изменять в инспекторе (пока ситуация такого не требует)
+    private float direction;
 
-    private void OnCollisionEnter2D(Collision2D col) // col - получаем ссылку на колайдер игрока, если скрипт висит на противнике и наоборот для игрока
+    private void OnCollisionStay2D(Collision2D col) // col - получаем ссылку на колайдер игрока, если скрипт висит на противнике и наоборот для игрока
     {
+        health = col.gameObject.GetComponent<Health>();
         if (col.gameObject.CompareTag(collisionTag))
         {
-            targetHealth = col.gameObject.GetComponent<Health>();
-          //  if (animator == null)
-                targetHealth.TakeHit(damage);
-
-            if (animator != null)
-                animator.SetTrigger("isCharacterTouched");
+            if (health != null)
+                direction = (col.transform.position - transform.position).x;
+                animator.SetFloat("Direction", Mathf.Abs(direction));
         }
     }
 
-    private void ApplyDamage()
+    private void SetDamage()
     {
-        if (targetHealth != null)
-        targetHealth.TakeHit(damage);
-
+        if (health != null)
+            health.TakeHit(damage);
+        health = null; //уничтожаем ссылку на объект, что бы не применять к нему эффект повторно
+        animator.SetFloat("Direction", 0f);
     }
 
     private void OnCollisionExit2D(Collision2D col)
