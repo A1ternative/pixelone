de3664ede97690a696c59fd34ebed230f10c68d0
A1ternative <alternative6556@gmail.com>
2019-11-02 21:46:35 +0400

    add new Item as ScriptableObject

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "New Entity/New Item")] // - атрибут, который позволяет создавать обьекты прямо в юнити
+// fileName - имя файла, которое будет присвоено нашему экземпляру ScriptableObject
+// menuName - путь к кнопке "создать наш айтем"
+public class Item : ScriptableObject
+{
+    // [SerializeField, HideInInspector] - если требуется сохранять какие либо обьекты сцены, но скрыть их редактирование из инспектора
+    [SerializeField] private int ID;
+    [SerializeField] private string itemName;
+    [SerializeField] private string description;
+    [SerializeField] private BuffType type;
+    [SerializeField] private float value;
+}
